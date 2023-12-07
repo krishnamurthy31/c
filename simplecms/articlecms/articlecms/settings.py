2e888b7a7bc1018cc876e499c135969aa1135154
krishnamurthy31 <130636039+krishnamurthy31@users.noreply.github.com>
2023-12-07 14:44:56 +0530

    first commit

---
+++ simplecms/articlecms/articlecms/settings.py
@@ -0,0 +1,23 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ALLOWED_HOSTS = ['your_domain_or_ip_here']
+
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATES_DIR],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
